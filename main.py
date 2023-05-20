c0c060f8d7d03139f9b02c3ccd58e60205978536
M0hammed-Sayed <115302230+M0hammed-Sayed@users.noreply.github.com>
2023-05-20 21:47:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,50 +1,63 @@
-import copy
-import math
+# Main game loop
+def play_game():
+    fun = 0
+    lEasy = 4
+    lMid = 6
+    lHard = 8
+    level = lEasy
+    current_player = PLAYER_X
+    while not is_game_over(board):
+        print_board(board)
+        if fun == 0:
+
+            if current_player == PLAYER_X:
+                print("Player Black's turn:")
+                # _, move = alpha_beta(board,0,0, 4, float('-inf'), float('inf'), True)
+                _, move = minimax(board, 0, 0, level, True)
+            else:
+                print("Player White's turn:")
+                # _, move = alpha_beta(board,0,0,4, float('-inf'), float('inf'), False)
+                _, move = minimax(board, 0, 0, level, False)
+            if move:
+                make_move(board, move[0], move[1], move[2], move[3], move[4])
+            else:
+                print("No valid moves.")
+                break
+
+            current_player = PLAYER_O if current_player == PLAYER_X else PLAYER_X
+
+            print_board(board)
+        else:
+
+            if current_player == PLAYER_X:
+                print("Player Black's turn:")
+                _, move = alpha_beta(board, 0, 0, level, float('-inf'), float('inf'), True)
+                # _, move = minimax(board,0,0, 4, True)
+            else:
+                print("Player White's turn:")
+                _, move = alpha_beta(board, 0, 0, level, float('-inf'), float('inf'), False)
+                # _, move = minimax(board,0,0,4, False)
+            if move:
+                make_move(board, move[0], move[1], move[2], move[3], move[4])
+            else:
+                print("No valid moves.")
+                break
+
+            current_player = PLAYER_O if current_player == PLAYER_X else PLAYER_X
+
+            print_board(board)
+    if is_game_over(board):
+        score_x = evaluate(board, 0, BLACK)
+        score_o = evaluate(board, 0, WHITE)
+        score_w = evaluate(board, 0, WKING)
+        score_b = evaluate(board, 0, BKING)
+        if score_x > score_o or score_b > score_w:
+            print("Player White wins!")
+        elif score_x < score_o or score_b < score_w:
+            print("Player Black wins!")
+        else:
+            print("It's a draw!")
 
 
-
-# Constants
-EMPTY = 0
-PLAYER_X = 1
-PLAYER_O = 2
-BLACK = 1
-WHITE = 2
-WKING = 3
-BKING = 4
-Capture = 0
-# Board size
-ROWS = 8
-COLS = 8
-
-# Initialize the board
-board = [
-    [0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [2, 0, 2, 0, 2, 0, 2, 0],
-    [0, 2, 0, 2, 0, 2, 0, 2],
-    [2, 0, 2, 0, 2, 0, 2, 0]
-]
-
-# Function to print the board
-def print_board(board):
-    print("   A B C D E F G H")
-    print("  ----------------")
-    for i in range(8):
-        print(i + 1, "|", end="  ")
-        for j in range(8):
-            piece = board[i][j]
-            if piece == EMPTY:
-                print(".", end="  ")
-            elif piece == BLACK:
-                print("B", end="  ")
-            elif piece == WHITE:
-                print("W", end="  ")
-            elif piece == WKING:
-                print("WK", end="  ")
-            elif piece == BKING:
-                print("BK", end="  ")    
-        print()
-    print()
+# Start the game
+play_game()
